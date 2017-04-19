5590e192ae7afc4ab656fac095682593b89ea793
JIANGYIFENGPI <472231536@qq.com>
2017-04-19 11:03:25 +0000

    first commit

--- bitwise.c
+++ bitwise.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 
 void displayBits(unsigned int value);
+void setbit(unsigned int *value, int position);
+void togglebit(unsigned int *value, int position);
 
 int main(void){
     unsigned int x;
@@ -8,12 +10,15 @@ int main(void){
     scanf("%u", &x);
 
     displayBits(x);
+    setbit(&x, 31);
+    displayBits(x);
+    togglebit(&x,20);
+    displayBits(x);
 }
 
-void displayBits(unsigned int value){
-    int i;
-    unsigned int displayMask = 1<<31;
-    printf("%10u = ",value);
+void setBits(unsigned int value){
+    unsigned int mask = 1<<pos;
+    *value = *value 
 
     for(i=1;i<=32;i++){
         putchar( value&displayMask ? '1':'0');
